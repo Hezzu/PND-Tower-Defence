47cb99c32f69e14d6a9a6c2654aecbbbe58d46c4
Hezzu <M3guM3gu@gmail.com>
2024-02-06 18:04:41 +0100

    Adding Tower wiki to active panels

--- Scenes/SupportScenes/enemy_info.gd
+++ Scenes/SupportScenes/enemy_info.gd
@@ -14,11 +14,12 @@ func _ready():
 	hpbar = $Panel/MarginContainer/VBoxContainer/ProgressBar
 
 func fillInfo(enemy):
-	unit.text = enemy.unit
-	hp.text = "HP: " + str(ceil(enemy.hp)) + "/" + str(ceil(enemy.maxHp))
-	hpbar.max_value = enemy.maxHp
-	hpbar.value = enemy.hp
-	if enemy.armor < 1:
-		armor.visible = true
-		armor.text = "Armor: " + str((1.0 - enemy.armor) * 100) + "%"
-	speed.text = "Speed: " + str(round(enemy.speed))
+	if enemy != null: 
+		unit.text = enemy.unit
+		hp.text = "HP: " + str(ceil(enemy.hp)) + "/" + str(ceil(enemy.maxHp))
+		hpbar.max_value = enemy.maxHp
+		hpbar.value = enemy.hp
+		if enemy.armor < 1:
+			armor.visible = true
+			armor.text = "Armor: " + str((1.0 - enemy.armor) * 100) + "%"
+		speed.text = "Speed: " + str(round(enemy.speed))
--- Scenes/UIScenes/shop.gd
+++ Scenes/UIScenes/shop.gd
@@ -1,8 +1,21 @@
 extends Panel
+var towerwiki = preload("res://Scenes/UIScenes/tower_info.tscn")
+var towerwikiopen = false
 
 var towerList = GameData.shopData
 @onready var diff = get_parent().get_parent().get_parent().diff
 
 func _ready():
 	for i in towerList:
-		get_node("Margin/ScrollContainer/BuilderMargin/Builder/" + GameData.towerData[i]["set"] + "/" + str(i)).text = str(round(towerList[i]["price"] * GameData.diffData[diff]["priceMod"])) + "$"
+		get_node("Margin/VBoxContainer/ScrollContainer/BuilderMargin/Builder/" + GameData.towerData[i]["set"] + "/" + str(i)).text = str(round(towerList[i]["price"] * GameData.diffData[diff]["priceMod"])) + "$"
+
+
+func _on_button_pressed():
+	var wiki = towerwiki.instantiate()
+	wiki.connect("leave", Callable(self, "wikileave").bind(wiki))
+	get_parent().add_child(wiki)
+	wiki.fillInfo("turret")
+	towerwikiopen = true
+
+func wikileave(obj):
+	obj.queue_free()
